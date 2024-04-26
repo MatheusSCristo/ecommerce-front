dab47eff4ef55990e38293fe558e1b35a422ef0c
Matheus <cristo.matheus@outlook.com>
2024-04-25 21:36:40 -0300

    feat:Add user to localStorage on auth

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,15 +1,33 @@
+"use client";
 import { User } from "@/types";
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 type ContextType = {
   user: User | null;
-  setUser: React.Dispatch<React.SetStateAction<User|null>>;
+  setUser: React.Dispatch<React.SetStateAction<User | null>>;
 };
 
 const UserContext = createContext<ContextType>({} as ContextType);
 
 function UserProvider({ children }: { children: React.ReactNode }) {
-  const [user, setUser] = useState<User | null >(null);
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    if(user){
+      localStorage.setItem("user", JSON.stringify(user));
+    }
+  }, [user]);
+
+  useEffect(() => {
+    setUser(getUserFromLocalStorage());
+  }, []);
+
+  function getUserFromLocalStorage() {
+    const local = localStorage.getItem("user");
+    if (local) return JSON.parse(local);
+    else return {} as User;
+  }
+
   return (
     <UserContext.Provider value={{ user, setUser }}>
       {children}
