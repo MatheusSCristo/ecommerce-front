cfd4f9547baf7b67a90605186b7140875dbc06ef
Matheus <cristo.matheus@outlook.com>
2024-04-29 21:53:30 -0300

    feat:Implements cartproduts  on localstorage

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,5 +1,5 @@
 import { CartProduct } from "@/types";
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 type ContextType = {
   products: CartProduct[];
@@ -10,6 +10,23 @@ const CartContext = createContext<ContextType>({} as ContextType);
 
 function CartProvider({ children }: { children: React.ReactNode }) {
   const [products, setProducts] = useState<CartProduct[]>([]);
+
+  const saveProductsOnLocal = () => {
+    if (products.length > 0)
+      localStorage.setItem("cartProducts", JSON.stringify(products));
+  };
+
+  useEffect(() => {
+    saveProductsOnLocal();
+  }, [products]);
+
+  useEffect(() => {
+    const local = localStorage.getItem("cartProducts");
+    if (local) {
+      setProducts(JSON.parse(local));
+    }
+  }, []);
+
   return (
     <CartContext.Provider value={{ products, setProducts }}>
       {children}
