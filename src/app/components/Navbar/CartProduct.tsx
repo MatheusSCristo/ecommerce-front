9f7ea8033ca7e7bdba1878988e48b5b8fd2868aa
Matheus <cristo.matheus@outlook.com>
2024-05-12 19:53:38 -0300

    refact: Change quantity button design on cart

--- src/app/components/Navbar/CartProduct.tsx
+++ src/app/components/Navbar/CartProduct.tsx
@@ -11,14 +11,13 @@ const CartProduct = ({ product }: Props) => {
   const [quantity, setQuantity] = useState(product.quantity);
   const { products, setProducts } = useContext(CartContext);
 
- 
   const handleQuantityChange = (quantity: number) => {
     let newProducts = [...products];
     if (quantity >= 1) {
       const index = newProducts.findIndex((item) => item.id == product.id);
       newProducts[index].quantity = quantity;
     } else {
-      newProducts=newProducts.filter((item) => item.id !== product.id);
+      newProducts = newProducts.filter((item) => item.id !== product.id);
     }
     setProducts(newProducts);
   };
@@ -28,9 +27,7 @@ const CartProduct = ({ product }: Props) => {
       <div className="flex gap-2 ">
         <div className="p-1   w-[60px] h-[60px] md:w-[100px] md:h-[100px]  relative">
           <Image
-            src={
-              product.imagesUrl[0] || "/images/notFoundImage.png"
-            }
+            src={product.imagesUrl[0] || "/images/notFoundImage.png"}
             alt=""
             fill
             className="object-contain rounded-sm"
@@ -39,19 +36,19 @@ const CartProduct = ({ product }: Props) => {
           />
         </div>
         <div className="flex flex-col gap-2">
-          <h1 className="text-sm md:text-md text-wrap w-[150px] ">{product.name}</h1>
+          <h1 className="text-sm md:text-md text-wrap w-[150px] ">
+            {product.name}
+          </h1>
           <div className="flex items-center gap-2 border-black border w-fit px-2">
-            {quantity > 1 && (
-              <button
-                className="text-gray-500"
-                onClick={() => {
-                  handleQuantityChange(quantity - 1);
-                  setQuantity((prevState) => prevState - 1);
-                }}
-              >
-                -
-              </button>
-            )}
+            <button
+              className="text-gray-500"
+              onClick={() => {
+                handleQuantityChange(quantity - 1);
+                setQuantity((prevState) => prevState - 1);
+              }}
+            >
+              -
+            </button>
             <h2>{quantity}</h2>
             <button
               className="text-gray-500"
